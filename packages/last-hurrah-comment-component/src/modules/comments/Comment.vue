d1a67f24ed507d794f5187910ccf4a0854bba070
scott <scottberry91@gmail.com>
2021-11-15 22:47:44 +0000

    added vue to web component library

---
+++ packages/last-hurrah-comment-component/src/modules/comments/Comment.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import CommentList from './containers/CommentList.vue'
+export default {
+    name: 'Comment',
+    setup() {},
+    components: {
+        CommentList,
+    },
+}
+</script>
+
+<template>
+    <h2>All Working, Still Working</h2>
+    <CommentList />
+</template>
+
+<style lang="scss"></style>
---
+++ packages/last-hurrah-comment-component/src/modules/comments/containers/CommentList.vue
@@ -0,0 +1,30 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+const comments = ref()
+
+export default defineComponent({
+    name: 'CommentList',
+    setup() {
+        const { result } = useQuery(gql`
+            query {
+                fetch_comments {
+                    comments {
+                        edited
+                        plain_text_body
+                        created_at
+                        updated_at
+                    }
+                }
+            }
+        `)
+
+        console.log(result)
+    },
+})
+</script>
