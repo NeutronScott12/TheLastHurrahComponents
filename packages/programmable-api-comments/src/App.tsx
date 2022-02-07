55cf8d4660194284ef92ea69c1011845ce32333a
scott <scottberry91@gmail.com>
2022-02-07 19:56:51 +0000

    started react project for programmable API component

---
+++ packages/programmable-api-comments/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CommentAPI } from '@thelasthurrah/comment_api'
+
+function App() {
+    return (
+        <div className="App">
+            <div>Test</div>
+        </div>
+    )
+}
+
+export default App
