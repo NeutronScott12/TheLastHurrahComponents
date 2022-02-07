f1c023a304a8db46b1436d1780190109403f245d
scott <scottberry91@gmail.com>
2022-02-07 20:56:27 +0000

    debating how to persist the cache with the programmable AI

--- packages/programmable-api-comments/src/App.tsx
+++ packages/programmable-api-comments/src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+
+import { useFormik } from 'formik'
 import { CommentAPI } from '@thelasthurrah/comment_api'
+import * as yup from 'yup'
+interface IFormValues {
+    body: string
+}
+
+const validationSchema = yup.object({
+    body: yup.string().required(),
+})
 
 function App() {
+    const formik = useFormik<IFormValues>({
+        initialValues: {
+            body: '',
+        },
+        validationSchema,
+        onSubmit() {
+            console.log('WORKING')
+        },
+    })
+
     return (
         <div className="App">
             <div>Test</div>
