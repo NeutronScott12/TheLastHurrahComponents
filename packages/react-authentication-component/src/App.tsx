7e391fb984db6dbcf9c55b6fcb1a67a1bc4d6cea
scott <scottberry91@gmail.com>
2022-05-31 15:25:17 +0100

    added react-authentication-component

---
+++ packages/react-authentication-component/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { AuthenticationContainer } from './library/src/AuthenticationContainer'
+
+function App() {
+    return (
+        <div className="App">
+            <AuthenticationContainer />
+        </div>
+    )
+}
+
+export default App
---
+++ packages/react-authentication-component/src/library/src/AuthenticationContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AuthenticationContainer = () => {
+    return (
+        <div>
+            <h1>Authentication Container</h1>
+        </div>
+    )
+}
